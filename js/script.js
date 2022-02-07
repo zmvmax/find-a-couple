b7b9940068da98974b1cea9f1296ff71a78e9558
EternalTempter <FrOsTiK12345678998@gmail.com>
2022-02-07 12:40:12 +0200

    m

---
+++ js/script.js
@@ -0,0 +1,9 @@
+'use strict';
+// let chooseModeForm = document.createElement('form');
+// let modeTimer = document.createElement('input');
+// let modeRecord = document.createElement('input');
+// modeTimer.setAttribute('type','radio');
+// modeRecord.setAttribute('type','radio');
+// chooseModeForm.appendChild(modeTimer);
+// chooseModeForm.appendChild(modeRecord);
+// document.body.appendChild(chooseModeForm);
